71f88bacf81d3db95cab3db4a091c7a602118e09
Tang-tang <Tang-tang@Tang-tangdeMac.local>
2015-07-15 19:27:58 -0700

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Tang-tang on 15-7-15.
+//  Copyright (c) 2015年 Tang-tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Tang-tang on 15-7-15.
+//  Copyright (c) 2015年 Tang-tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
